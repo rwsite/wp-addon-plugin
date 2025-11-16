3393bb9c0fa89681ce9dbb4474623e71687d97cc
Алексей Тихомиров <alex@rwsite.ru>
2025-11-16 15:35:22 +0300

    Изменено название плагина на WP Excellence Addon

--- wp-addon-plugin.php
+++ wp-addon-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:  WP Excellence Add-In
+ * Plugin Name:  WP Excellence Addon
  * Plugin URL:   https://rwsite.ru
  * Description:  Transforms your standard WordPress installation into an excellent, optimized website with comprehensive performance, security, and usability enhancements.
  * Version:      1.3.2
